70867f5dbcf32971abb07c0206c1ab59b1b3e1c0
mcsrainbow <guosuiyu@gmail.com>
2013-07-18 02:19:12 +0000

    Update PATH settings

--- service/global_settings.py
+++ service/global_settings.py
@@ -5,5 +5,5 @@
 # Date: Fri 29 Mar 2013 10:37:48 AM CST
 # Author: Dong Guo
 
-PROJECT_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep"
-CONFIG_HOME = "/home/dong.guo/github/HeyDevOps/Python/projects/ddep/conf"
+PROJECT_HOME = "/home/dong.guo/workspace/github/python-projects-ddep/ddep"
+CONFIG_HOME = "/home/dong.guo/workspace/github/python-projects-ddep/ddep/conf"
